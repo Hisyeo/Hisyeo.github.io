2eb9525922bbdfec6c4db173534f5d017831ac64
Jake Russo <madcap.russo@gmail.com>
2024-02-01 09:05:29 -0600

    revert change and try to tweak name in plugin config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,7 +28,7 @@ const config = {
     ['@lunaticmuch/docusaurus-terminology', {
       termsDir: './docs/words/',
       termsUrl: "/docs/words",
-      glossaryFilepath: './docs/lexicon.md',
+      glossaryFilepath: './docs/Lexicon.md',
     }]
   ],
   // Even if you don't use internalization, you can use this field to set useful
