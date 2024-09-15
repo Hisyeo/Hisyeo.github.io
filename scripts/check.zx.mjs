164a78ffb3fedac7ffcc7a0180f72e97980fd3cc
Jake Russo <madcap.russo@gmail.com>
2024-09-15 08:18:50 -0500

    clean up monophthongize function

--- scripts/check.zx.mjs
+++ scripts/check.zx.mjs
@@ -176,148 +176,82 @@ function neutralize(word) {
 }
 
 function monophthongize(word) {
+    const smoothL = (ls, i) => `${ls.slice(0,i).join('')}${ls.slice(i+1).join('')}`;
+    const smoothR = (ls, i) => `${ls.slice(0,i+1).join('')}${ls.slice(i+2).join('')}`;
     const letters = word.split('');
     for (let index = 0; index < letters.length; index++) {
-        const [letterA, letterB] = [letters[index], letters[index + 1]];
-        // console.log(`Letter A: ${letterA}\tLetter B: ${letterB}`);
-        if (false) {
+        switch(`${letters[index]}${letters[index + 1]}`) {
+        case 'oo': console.error(`Illegal syllable in ${word}`)
+        case 'oö': return smoothR(letters, index)
+        case 'oe': console.error(`Illegal syllable in ${word}`)
+        case 'oë': return smoothR(letters, index)
+        case 'oı': return smoothR(letters, index)
+        case 'oi': return smoothR(letters, index)
+        case 'ou': console.error(`Illegal syllable in ${word}`)
+        case 'oü': return smoothR(letters, index)
         
-        } else if (letterA == 'o' && letterB == 'o') {
-            console.error(`Illegal syllable in ${word}`)
-        } else if (letterA == 'o' && letterB == 'ö') {
-            return `${letters.slice(0,index+1).join('')}${letters.slice(index+2).join('')}`
-        } else if (letterA == 'o' && letterB == 'e') {
-            console.error(`Illegal syllable in ${word}`)
-        } else if (letterA == 'o' && letterB == 'ë') {
-            console.error(`Illegal syllable in ${word}`)
-        } else if (letterA == 'o' && letterB == 'ı') {
-            return `${letters.slice(0,index+1).join('')}${letters.slice(index+2).join('')}`
-        } else if (letterA == 'o' && letterB == 'i') {
-            return `${letters.slice(0,index+1).join('')}${letters.slice(index+2).join('')}`
-        } else if (letterA == 'o' && letterB == 'u') {
-            console.error(`Illegal syllable in ${word}`)
-        } else if (letterA == 'o' && letterB == 'ü') {
-            return `${letters.slice(0,index+1).join('')}${letters.slice(index+2).join('')}`
+        case 'öo': return smoothR(letters, index)
+        case 'öö': console.error(`Illegal syllable in ${word}`)
+        case 'öe': return smoothR(letters, index)
+        case 'öë': return smoothR(letters, index)
+        case 'öı': return smoothR(letters, index)
+        case 'öi': return smoothR(letters, index)
+        case 'öu': return smoothR(letters, index)
+        case 'öü': return smoothR(letters, index)
         
-        } else if (letterA == 'ö' && letterB == 'o') {
-            return `${letters.slice(0,index+1).join('')}${letters.slice(index+2).join('')}`
-        } else if (letterA == 'ö' && letterB == 'ö') {
-            console.error(`Illegal syllable in ${word}`)
-        } else if (letterA == 'ö' && letterB == 'e') {
-            return `${letters.slice(0,index+1).join('')}${letters.slice(index+2).join('')}`
-        } else if (letterA == 'ö' && letterB == 'ë') {
-            return `${letters.slice(0,index+1).join('')}${letters.slice(index+2).join('')}`
-        } else if (letterA == 'ö' && letterB == 'ı') {
-            return `${letters.slice(0,index+1).join('')}${letters.slice(index+2).join('')}`
-        } else if (letterA == 'ö' && letterB == 'i') {
-            return `${letters.slice(0,index+1).join('')}${letters.slice(index+2).join('')}`
-        } else if (letterA == 'ö' && letterB == 'u') {
-            return `${letters.slice(0,index+1).join('')}${letters.slice(index+2).join('')}`
-        } else if (letterA == 'ö' && letterB == 'ü') {
-            return `${letters.slice(0,index+1).join('')}${letters.slice(index+2).join('')}`
+        case 'eo': console.error(`Illegal syllable in ${word}`)
+        case 'eö': return smoothR(letters, index)
+        case 'ee': console.error(`Illegal syllable in ${word}`)
+        case 'eë': console.error(`Illegal syllable in ${word}`)
+        case 'eı': return smoothR(letters, index)
+        case 'ei': return smoothR(letters, index)
+        case 'eu': console.error(`Illegal syllable in ${word}`)
+        case 'eü': return smoothR(letters, index)
+
+        case 'ëo': return smoothR(letters, index)
+        case 'ëö': return smoothR(letters, index)
+        case 'ëe': console.error(`Illegal syllable in ${word}`)
+        case 'ëë': console.error(`Illegal syllable in ${word}`)
+        case 'ëı': return smoothR(letters, index)
+        case 'ëi': return smoothR(letters, index)
+        case 'ëu': return smoothR(letters, index)
+        case 'ëü': return smoothR(letters, index)
         
-        } else if (letterA == 'e' && letterB == 'o') {
-            console.error(`Illegal syllable in ${word}`)
-        } else if (letterA == 'e' && letterB == 'ö') {
-            return `${letters.slice(0,index+1).join('')}${letters.slice(index+2).join('')}`
-        } else if (letterA == 'e' && letterB == 'e') {
-            console.error(`Illegal syllable in ${word}`)
-        } else if (letterA == 'e' && letterB == 'ë') {
-            console.error(`Illegal syllable in ${word}`)
-        } else if (letterA == 'e' && letterB == 'ı') {
-            return `${letters.slice(0,index+1).join('')}${letters.slice(index+2).join('')}`
-        } else if (letterA == 'e' && letterB == 'i') {
-            return `${letters.slice(0,index+1).join('')}${letters.slice(index+2).join('')}`
-        } else if (letterA == 'e' && letterB == 'u') {
-            console.error(`Illegal syllable in ${word}`)
-        } else if (letterA == 'e' && letterB == 'ü') {
-            return `${letters.slice(0,index+1).join('')}${letters.slice(index+2).join('')}`
+        case 'ıo': console.error(`Illegal syllable in ${word}`)
+        case 'ıö': return smoothL(letters, index)
+        case 'ıe': console.error(`Illegal syllable in ${word}`)
+        case 'ıë': console.error(`Illegal syllable in ${word}`)
+        case 'ıı': console.error(`Illegal syllable in ${word}`)
+        case 'ıi': console.error(`Illegal syllable in ${word}`)
+        case 'ıu': console.error(`Illegal syllable in ${word}`)
+        case 'ıü': return smoothL(letters, index)
 
-        } else if (letterA == 'ë' && letterB == 'o') {
-            return `${letters.slice(0,index+1).join('')}${letters.slice(index+2).join('')}`
-        } else if (letterA == 'ë' && letterB == 'ö') {
-            return `${letters.slice(0,index+1).join('')}${letters.slice(index+2).join('')}`
-        } else if (letterA == 'ë' && letterB == 'e') {
-            console.error(`Illegal syllable in ${word}`)
-        } else if (letterA == 'ë' && letterB == 'ë') {
-            console.error(`Illegal syllable in ${word}`)
-        } else if (letterA == 'ë' && letterB == 'ı') {
-            return `${letters.slice(0,index+1).join('')}${letters.slice(index+2).join('')}`
-        } else if (letterA == 'ë' && letterB == 'i') {
-            return `${letters.slice(0,index+1).join('')}${letters.slice(index+2).join('')}`
-        } else if (letterA == 'ë' && letterB == 'u') {
-            return `${letters.slice(0,index+1).join('')}${letters.slice(index+2).join('')}`
-        } else if (letterA == 'ë' && letterB == 'ü') {
+        case 'io': return smoothL(letters, index)
+        case 'iö': return smoothL(letters, index)
+        case 'ie': return smoothL(letters, index)
+        case 'ië': return smoothL(letters, index)
+        case 'iı': return smoothL(letters, index)
+        case 'ii': console.error(`Illegal syllable in ${word}`)
+        case 'iu': return smoothL(letters, index)
+        case 'iü': return smoothL(letters, index)
 
-
-        } else if (letterA == 'ı' && letterB == 'o') {
-            console.error(`Illegal syllable in ${word}`)
-        } else if (letterA == 'ı' && letterB == 'ö') {
-            return `${letters.slice(0,index).join('')}${letters.slice(index+1).join('')}`
-        } else if (letterA == 'ı' && letterB == 'e') {
-            console.error(`Illegal syllable in ${word}`)
-        } else if (letterA == 'ı' && letterB == 'ë') {
-            console.error(`Illegal syllable in ${word}`)
-        } else if (letterA == 'ı' && letterB == 'ı') {
-            console.error(`Illegal syllable in ${word}`)
-        } else if (letterA == 'ı' && letterB == 'i') {
-            console.error(`Illegal syllable in ${word}`)
-        } else if (letterA == 'ı' && letterB == 'u') {
-            console.error(`Illegal syllable in ${word}`)
-        } else if (letterA == 'ı' && letterB == 'ü') {
-            return `${letters.slice(0,index).join('')}${letters.slice(index+1).join('')}`
-
-        } else if (letterA == 'i' && letterB == 'o') {
-            return `${letters.slice(0,index).join('')}${letters.slice(index+1).join('')}`
-        } else if (letterA == 'i' && letterB == 'ö') {
-            return `${letters.slice(0,index).join('')}${letters.slice(index+1).join('')}`
-        } else if (letterA == 'i' && letterB == 'e') {
-            return `${letters.slice(0,index).join('')}${letters.slice(index+1).join('')}`
-        } else if (letterA == 'i' && letterB == 'ë') {
-            return `${letters.slice(0,index).join('')}${letters.slice(index+1).join('')}`
-        } else if (letterA == 'i' && letterB == 'ı') {
-            return `${letters.slice(0,index).join('')}${letters.slice(index+1).join('')}`
-        } else if (letterA == 'i' && letterB == 'i') {
-            console.error(`Illegal syllable in ${word}`)
-        } else if (letterA == 'i' && letterB == 'u') {
-            return `${letters.slice(0,index).join('')}${letters.slice(index+1).join('')}`
-        } else if (letterA == 'i' && letterB == 'ü') {
-            return `${letters.slice(0,index).join('')}${letters.slice(index+1).join('')}`
-
-        } else if (letterA == 'u' && letterB == 'o') {
-            console.error(`Illegal syllable in ${word}`)
-        } else if (letterA == 'u' && letterB == 'ö') {
-            return `${letters.slice(0,index+1).join('')}${letters.slice(index+2).join('')}`
-        } else if (letterA == 'u' && letterB == 'e') {
-            console.error(`Illegal syllable in ${word}`)
-        } else if (letterA == 'u' && letterB == 'ë') {
-            return `${letters.slice(0,index+1).join('')}${letters.slice(index+2).join('')}`
-        } else if (letterA == 'u' && letterB == 'ı') {
-            console.error(`Illegal syllable in ${word}`)
-        } else if (letterA == 'u' && letterB == 'i') {
-            return `${letters.slice(0,index+1).join('')}${letters.slice(index+2).join('')}`
-        } else if (letterA == 'u' && letterB == 'u') {
-            console.error(`Illegal syllable in ${word}`)
-        } else if (letterA == 'u' && letterB == 'ü') {
-            return `${letters.slice(0,index+1).join('')}${letters.slice(index+2).join('')}`
+        case 'uo': console.error(`Illegal syllable in ${word}`)
+        case 'uö': return smoothR(letters, index)
+        case 'ue': console.error(`Illegal syllable in ${word}`)
+        case 'uë': return smoothR(letters, index)
+        case 'uı': console.error(`Illegal syllable in ${word}`)
+        case 'ui': return smoothR(letters, index)
+        case 'uu': console.error(`Illegal syllable in ${word}`)
+        case 'uü': return smoothR(letters, index)
         
-
-        } else if (letterA == 'ü' && letterB == 'o') {
-            return `${letters.slice(0,index).join('')}${letters.slice(index+1).join('')}`
-        } else if (letterA == 'ü' && letterB == 'ö') {
-            return `${letters.slice(0,index).join('')}${letters.slice(index+1).join('')}`
-        } else if (letterA == 'ü' && letterB == 'e') {
-            return `${letters.slice(0,index).join('')}${letters.slice(index+1).join('')}`
-        } else if (letterA == 'ü' && letterB == 'ë') {
-            return `${letters.slice(0,index).join('')}${letters.slice(index+1).join('')}`
-        } else if (letterA == 'ü' && letterB == 'ı') {
-            return `${letters.slice(0,index).join('')}${letters.slice(index+1).join('')}`
-        } else if (letterA == 'ü' && letterB == 'i') {
-            return `${letters.slice(0,index).join('')}${letters.slice(index+1).join('')}`
-        } else if (letterA == 'ü' && letterB == 'u') {
-            return `${letters.slice(0,index).join('')}${letters.slice(index+1).join('')}`
-        } else if (letterA == 'ü' && letterB == 'ü') {
-            console.error(`Illegal syllable in ${word}`)
+        case 'üo': return smoothL(letters, index)
+        case 'üö': return smoothL(letters, index)
+        case 'üe': return smoothL(letters, index)
+        case 'üë': return smoothL(letters, index)
+        case 'üı': return smoothL(letters, index)
+        case 'üi': return smoothL(letters, index)
+        case 'üu': return smoothL(letters, index)
+        case 'üü': console.error(`Illegal syllable in ${word}`)
         }
     }
 }
