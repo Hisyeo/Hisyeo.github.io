eddaf7d6573eee1a8b6c28725e7ef90a64cfe53a
Jake Russo <madcap.russo@gmail.com>
2024-09-18 21:53:20 -0500

    improve collision check script

--- scripts/check.zx.mjs
+++ scripts/check.zx.mjs
@@ -38,6 +38,7 @@ const minPairs = {
 // const badSyllablesMessage = "Some words have improper syllable structure"
 const syllableRegex = /(?<onset>[ꞌhkgtcxsdzpvmnlwy])?(?<nucleus>[oöeëıiuü])(?<coda>[tkscnl](?![oöeëıiuü]))?/gi
 
+
 const records = await csv(file, {
     delimiter: ',',
     columns: true,
@@ -54,17 +55,37 @@ const collisions = []
 for (const record of records) {
     words.push(record["Hisyëö"])
     neutrals.push(neutralize(record["Hisyëö"]))
-    monophthongs[monophthongize(record["Hisyëö"])] = record["Hisyëö"]
-    if (/[oöeëıiuü]/.test(record["Hisyëö"][0])) {
-        apocopes.push(`l${record["Hisyëö"]}`);
-        apocopes.push(`p${record["Hisyëö"]}`);
+    const smoothed = monophthongize(record["Hisyëö"])
+    if (smoothed != undefined)
+        monophthongs[smoothed] = [...monophthongs[smoothed] ?? [], record["Hisyëö"]];
+    if (![ 'Preposition',
+           'Conjunction',
+           'Postposition',
+           'Interjection'
+        ].includes(record["Type"]) &&
+        /[oöeëıiuü]/.test(record["Hisyëö"][0])) {
+        apocopes.push(`l${record["Hisyëö"]}`, `p${record["Hisyëö"]}`);
     }
 }
 
 console.log('Reviewing collisions...')
-    
-for (let word of words) {
+ 
+for (const smooth in monophthongs) {
+    if (words.includes(smooth)) {
+        collisionFound('Smoothed', smooth, monophthongs[smooth][0], 0, 0);
+    } else if (monophthongs[smooth].length > 1) {
+        collisionFound('Smoothed', monophthongs[smooth][0], monophthongs[smooth][1], 0, 0);
+    }
+}
 
+for (const apocope of apocopes) {
+    const wordIndex = words.indexOf(apocope);
+    if (wordIndex >= 0) {
+        collisionFound('Apocopic', words[wordIndex], `${apocope[0]}'${apocope.slice(1)}`, 0, 0)
+    }
+}
+
+for (let word of words) {
 
     // Syllable comparisons
     const matches = word.matchAll(syllableRegex)
@@ -73,13 +94,7 @@ for (let word of words) {
     Array.from(matches).forEach((m) => {
         sylSegments.push(m.groups); sylValues.push(m[0])
     })
-    // if (sylValues.filter((s) => s.length < 2).length > 1) {
-    //     if (!badSyllablesIdentified) {
-    //         console.log(badSyllablesMessage)
-    //         badSyllablesIdentified = true
-    //     }
-    //     console.log(`word:${word}\tmatches: ${sylValues}`)
-    // }
+
     sylSegments.forEach((syllable, i) => {
         let {onset, nucleus, coda} = syllable
         if (!onset) onset = "'"
@@ -88,20 +103,20 @@ for (let word of words) {
             collSyl[i] = makeSyllable(collider, nucleus, coda)
             let wordOnsetChanged = collSyl.join('')
             if (words.includes(wordOnsetChanged)) {
-                collisionFound('Consonant', word, wordOnsetChanged, hl(collSyl, i), hl(collSyl, i))
-            } else if (monophthongs[wordOnsetChanged]) {
-                collisionFound('Smoothed', word, monophthongs[wordOnsetChanged], hl(collSyl, i), hl(collSyl, i))
+                collisionFound('Forbidden Pair', word, wordOnsetChanged, hl(collSyl, i), hl(collSyl, i))
+            } else if (monophthongs[wordOnsetChanged]?.length > 0) {
+                collisionFound('Smoothed Forbidden Pair', word, monophthongs[wordOnsetChanged][0], hl(collSyl, i), hl(collSyl, i))
             } else if (apocopes.includes(wordOnsetChanged)) {
-                collisionFound('Apocopic', word, `${wordOnsetChanged[0]}'${wordOnsetChanged.slice(1)}`, hl(collSyl, i), hl(collSyl, i) + 1)
+                collisionFound('Apocopic Forbidden Pair', word, `${wordOnsetChanged[0]}'${wordOnsetChanged.slice(1)}`, hl(collSyl, i), hl(collSyl, i) + 1)
             } else if (neutrals.findIndex(w => w == neutralize(wordOnsetChanged)) != -1) {
-                collisionFound('Neutralized', word, wordOnsetChanged, hl(collSyl, i), hl(collSyl, i))
+                collisionFound('Neutralized Forbidden Pair', word, wordOnsetChanged, hl(collSyl, i), hl(collSyl, i))
             }
         }
         for (let collider of minPairs[nucleus]) {
             let collSyl = sylValues.slice()
             collSyl[i] = makeSyllable(onset, collider, coda)
             const nucleChange = collSyl.join('')
-            if (words.includes(nucleChange)) collisionFound('Vowel', word, nucleChange, 0)
+            if (words.includes(nucleChange)) collisionFound('Vowel Forbidden Pair', word, nucleChange, 0, 0)
         }
     })
 
@@ -160,7 +175,7 @@ function collisionFound(type, word, collision, locationA, locationB) {
 
 function priorLength(syllables, i) {
     let count = 0
-    for (const syllable of syllables) count = count + syllable.length
+    for (const syllable of syllables) count = count + syllable.length;
     return count
 }
 
@@ -176,81 +191,89 @@ function neutralize(word) {
 }
 
 function monophthongize(word) {
-    const smoothL = (ls, i) => `${ls.slice(0,i).join('')}${ls.slice(i+1).join('')}`;
-    const smoothR = (ls, i) => `${ls.slice(0,i+1).join('')}${ls.slice(i+2).join('')}`;
+    const smoothL = (ls, i) => {
+        let result = `${ls.slice(0,i+1).join('')}${ls.slice(i+2).join('')}`;
+        // console.log(`word: ${word}\tresult: ${result}`);
+        return result
+    }
+    const smoothR = (ls, i) => {
+        let result = `${ls.slice(0,i).join('')}${ls.slice(i+1).join('')}`;
+        // console.log(`word: ${word}\tresult: ${result}`);
+        return result
+    }
     const letters = word.split('');
     for (let index = 0; index < letters.length; index++) {
         switch(`${letters[index]}${letters[index + 1]}`) {
         case 'oo': console.error(`Illegal syllable in ${word}`)
-        case 'oö': return smoothR(letters, index)
+        case 'oö': return smoothL(letters, index)
         case 'oe': console.error(`Illegal syllable in ${word}`)
-        case 'oë': return smoothR(letters, index)
-        case 'oı': return smoothR(letters, index)
-        case 'oi': return smoothR(letters, index)
+        case 'oë': return smoothL(letters, index)
+        case 'oı': return smoothL(letters, index)
+        case 'oi': return smoothL(letters, index)
         case 'ou': console.error(`Illegal syllable in ${word}`)
-        case 'oü': return smoothR(letters, index)
+        case 'oü': return smoothL(letters, index)
         
-        case 'öo': return smoothR(letters, index)
+        case 'öo': return smoothL(letters, index)
         case 'öö': console.error(`Illegal syllable in ${word}`)
-        case 'öe': return smoothR(letters, index)
-        case 'öë': return smoothR(letters, index)
-        case 'öı': return smoothR(letters, index)
-        case 'öi': return smoothR(letters, index)
-        case 'öu': return smoothR(letters, index)
-        case 'öü': return smoothR(letters, index)
+        case 'öe': return smoothL(letters, index)
+        case 'öë': return smoothL(letters, index)
+        case 'öı': return smoothL(letters, index)
+        case 'öi': return smoothL(letters, index)
+        case 'öu': return smoothL(letters, index)
+        case 'öü': return smoothL(letters, index)
         
         case 'eo': console.error(`Illegal syllable in ${word}`)
-        case 'eö': return smoothR(letters, index)
+        case 'eö': return smoothL(letters, index)
         case 'ee': console.error(`Illegal syllable in ${word}`)
         case 'eë': console.error(`Illegal syllable in ${word}`)
-        case 'eı': return smoothR(letters, index)
-        case 'ei': return smoothR(letters, index)
+        case 'eı': return smoothL(letters, index)
+        case 'ei': return smoothL(letters, index)
         case 'eu': console.error(`Illegal syllable in ${word}`)
-        case 'eü': return smoothR(letters, index)
+        case 'eü': return smoothL(letters, index)
 
         case 'ëo': return smoothR(letters, index)
         case 'ëö': return smoothR(letters, index)
         case 'ëe': console.error(`Illegal syllable in ${word}`)
         case 'ëë': console.error(`Illegal syllable in ${word}`)
-        case 'ëı': return smoothR(letters, index)
-        case 'ëi': return smoothR(letters, index)
+        case 'ëı': return smoothL(letters, index)
+        case 'ëi': return smoothL(letters, index)
         case 'ëu': return smoothR(letters, index)
         case 'ëü': return smoothR(letters, index)
         
         case 'ıo': console.error(`Illegal syllable in ${word}`)
-        case 'ıö': return smoothL(letters, index)
+        case 'ıö': return smoothR(letters, index)
         case 'ıe': console.error(`Illegal syllable in ${word}`)
         case 'ıë': console.error(`Illegal syllable in ${word}`)
         case 'ıı': console.error(`Illegal syllable in ${word}`)
         case 'ıi': console.error(`Illegal syllable in ${word}`)
         case 'ıu': console.error(`Illegal syllable in ${word}`)
-        case 'ıü': return smoothL(letters, index)
+        case 'ıü': return smoothR(letters, index)
 
-        case 'io': return smoothL(letters, index)
-        case 'iö': return smoothL(letters, index)
-        case 'ie': return smoothL(letters, index)
-        case 'ië': return smoothL(letters, index)
-        case 'iı': return smoothL(letters, index)
+        case 'io': return smoothR(letters, index)
+        case 'iö': return smoothR(letters, index)
+        case 'ie': return smoothR(letters, index)
+        case 'ië': return smoothR(letters, index)
+        case 'iı': return smoothR(letters, index)
         case 'ii': console.error(`Illegal syllable in ${word}`)
-        case 'iu': return smoothL(letters, index)
-        case 'iü': return smoothL(letters, index)
+        case 'iu': return smoothR(letters, index)
+        case 'iü': return smoothR(letters, index)
 
         case 'uo': console.error(`Illegal syllable in ${word}`)
         case 'uö': return smoothR(letters, index)
         case 'ue': console.error(`Illegal syllable in ${word}`)
-        case 'uë': return smoothR(letters, index)
+        case 'uë': return smoothL(letters, index)
         case 'uı': console.error(`Illegal syllable in ${word}`)
-        case 'ui': return smoothR(letters, index)
+        case 'ui': return smoothL(letters, index)
         case 'uu': console.error(`Illegal syllable in ${word}`)
-        case 'uü': return smoothR(letters, index)
+        case 'uü': return smoothL(letters, index)
         
-        case 'üo': return smoothL(letters, index)
-        case 'üö': return smoothL(letters, index)
-        case 'üe': return smoothL(letters, index)
-        case 'üë': return smoothL(letters, index)
-        case 'üı': return smoothL(letters, index)
-        case 'üi': return smoothL(letters, index)
-        case 'üu': return smoothL(letters, index)
+        case 'üo': return smoothR(letters, index)
+        case 'üö': return smoothR(letters, index)
+        case 'üe': return smoothR(letters, index)
+        case 'üë': return smoothR(letters, index)
+        case 'üı': return smoothR(letters, index)
+        case 'üi': return smoothR(letters, index)
+        case 'üu': return smoothR(letters, index)
         case 'üü': console.error(`Illegal syllable in ${word}`)
         }
     }
