caccce42ab95a061e2899a516dee6c638047aa3a
Jake Russo <madcap.russo@gmail.com>
2025-06-24 18:23:31 -0500

    Update homepage to include broader scope of project

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,35 +4,40 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Succinct',
+    title: 'Language',
     Svg: require('@site/static/img/undraw_everyday_life_re_1lfb.svg').default,
     description: (
       <>
-        There are under 900 words to learn. Not as small as Kokanu or Toki Pona
-        but much smaller than Globasa, Lidepla and Esperanto. A tiny fraction of
-        the lexicon sizes of natural languages.
+        There are only 866 words and they don't mutate, similar
+        to <a href="https://tokipona.org">
+          Toki Pona
+        </a> and <a href="https://kokanu.com">
+          Kokanu
+        </a> but with a more natural grammar and lexicon.
       </>
     ),
   },
   {
-    title: 'Expansive',
+    title: 'Community',
     Svg: require('@site/static/img/undraw_trip_re_f724.svg').default,
     description: (
       <>
-        Words are considered based upon the length and comprehensibility of
-        existing phrases that represent them, the commonality of the concept
-        itself, and the usefulness within other compound phrases.
+        Join a group of Hîsyêô-speaking people creating an organization
+        of <a href="https://participatoryeconomy.org">
+            worker councils
+        </a> and <a href="https://www.thefec.org">
+            intentional communites
+        </a>.
       </>
     ),
   },
   {
-    title: 'Expressive',
+    title: 'Culture',
     Svg: require('@site/static/img/undraw_book_reading_re_fu2c.svg').default,
     description: (
       <>
-        The grammar has the capability of formulating complex meanings with the
-        limited vocabulary whilst maintaining shorter sentences and limited
-        nested clauses.
+        A fully-fledged set of beliefs, idioms, & festivals. Values of
+        liberty, justice, and participation for all sapient life.
       </>
     ),
   },
