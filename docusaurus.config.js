f50c3cbc480846f57269d3b52045c76fc6b8739b
MadcapJake <madcap.russo@gmail.com>
2025-12-28 21:03:11 -0600

    Silence truncated blog post warnings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,6 +51,7 @@ const config = {
         },
         blog: {
           showReadingTime: true,
+          onUntruncatedBlogPosts: 'ignore',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
